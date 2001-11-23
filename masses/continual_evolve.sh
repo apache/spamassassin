fb0d82108ff719aff31f0dc7499e6d3abfcde1fc
jmason <jmason@unknown>
2001-11-23 05:08:50 +0000

    avoid overwrites
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@1731 13f79535-47bb-0310-9956-ffa450edef68

--- masses/continual_evolve.sh
+++ masses/continual_evolve.sh
@@ -3,10 +3,16 @@
 count=1
 echo $$ > cont_evolve.pid
 
+while [ -f result.$count ] ; do
+  count=`expr $count + 1`
+done
+
+echo "restarting evolve at result.$count"
+
 while true ; do
   ./evolve $*
   mv results.evolved result.$count 
-  echo "Copied to results.$count"
+  echo "Copied to result.$count"
   count=`expr $count + 1`
 done
 
