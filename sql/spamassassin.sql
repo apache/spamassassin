cee787ed22dd4e1d172ef0bdedab384443d310c2
jmason <jmason@unknown>
2001-12-03 03:28:57 +0000

    added indexed username to SQL, from Jeremy D. Zawodny
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@1775 13f79535-47bb-0310-9956-ffa450edef68

--- sql/spamassassin.sql
+++ sql/spamassassin.sql
@@ -3,6 +3,7 @@ CREATE TABLE userpref (
   preference varchar(30) NOT NULL,
   value varchar(100) NOT NULL,
   prefid int(11) NOT NULL auto_increment,
-  PRIMARY KEY (prefid)
+  PRIMARY KEY (prefid),
+  INDEX (username)
 ) TYPE=MyISAM;
 
