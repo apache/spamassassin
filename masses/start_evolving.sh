be15252f904c51a4f1072827b84a12e822417984
jmason <jmason@apache.org>
2001-12-19 02:57:47 +0000

    added cron scripts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@1917 13f79535-47bb-0310-9956-ffa450edef68

---
+++ masses/start_evolving.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /home/jm/ftp/spamassassin/masses
+(
+scp firewall:logs.gz logs.gz
+tar xvfz logs.gz && rm -f logs.gz
+
+make clean
+make evolve && ./continual_evolve.sh -s 1000 -b 20.0 -c 1.001
+)  > log 2>&1
+
+
+# crontab: start at 1am, stop at 9am
+# 3 1 * * *    /home/jm/ftp/spamassassin/masses/start_evolving.sh
+# 3 9 * * *    /home/jm/ftp/spamassassin/masses/stop_evolving.sh
+
