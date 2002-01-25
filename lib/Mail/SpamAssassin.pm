63574bf7f44a62c7af60f6c4bd9221a0c9c568ad
jmason <jmason@unknown>
2002-01-25 04:44:55 +0000

    2.01 RELEASED, bugfix
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@2075 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -68,8 +68,8 @@ use vars	qw{
 
 @ISA = qw();
 
-$VERSION = "2.01";
-$SUB_VERSION = 'devel $Id: SpamAssassin.pm,v 1.61 2002/01/25 04:41:02 jmason Exp $';
+$VERSION = "2.1";
+$SUB_VERSION = 'devel $Id: SpamAssassin.pm,v 1.62 2002/01/25 04:44:55 jmason Exp $';
 
 sub Version { $VERSION; }
 
