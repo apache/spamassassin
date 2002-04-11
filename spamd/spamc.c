8aaf3732f19b21b5dff708843d720d458818615f
hughescr <hughescr@apache.org>
2002-04-11 06:33:50 +0000

    spamc conditional compile for Digital Unix
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@2275 13f79535-47bb-0310-9956-ffa450edef68

--- spamd/spamc.c
+++ spamd/spamc.c
@@ -35,7 +35,9 @@
 #endif
 
 /* SunOS 4.1.4 patch from Tom Lipkis <tal@pss.com> */
-#if (defined(__sun__) && defined(__sparc__) && !defined(__svr4__)) || (defined(_IRIX_))
+#if (defined(__sun__) && defined(__sparc__) && !defined(__svr4__)) /* SunOS */
+     || (defined(_IRIX_))  /* IRIX */
+     || (defined(__osf__)) /* Digital UNIX */
 # ifndef EX__MAX
 # define EX__MAX 77
 extern char *optarg;
