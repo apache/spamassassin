50648ffc7510fb492508e07f199dae1ce5befc82
Duncan A. Findlay <duncf@apache.org>
2002-06-20 01:55:11 +0000

    Update Debian scripts for spamd. PRobably not necessary - people ought to use my packages\!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@2568 13f79535-47bb-0310-9956-ffa450edef68

--- spamd/debian-rc-script.sh
+++ spamd/debian-rc-script.sh
@@ -1,20 +1,25 @@
 #! /bin/sh
 
 # Spamd init script
-# November 2001
+# June 2002
 # Duncan Findlay
 
 # Based on skeleton by Miquel van Smoorenburg and Ian Murdock
 
-# Please do not name this script /etc/init.d/spamd.  It may cause problems.
-
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
-DAEMON=spamd
-PNAME=spamd
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/sbin/spamd
 NAME=spamd
-DESC="SpamAssasin Mail Filter Daemon"
+SNAME=spamassassin
+DESC="SpamAssassin Mail Filter Daemon"
+PIDFILE="/var/run/$NAME.pid"
+PNAME="spamd"
+DOPTIONS="-d --pidfile=$PIDFILE"
 
-test -r /etc/spamd.conf && . /etc/spamd.conf
+# Defaults - don't touch, edit /etc/default/spamassassin
+ENABLED=0
+OPTIONS=""
+
+test -f /etc/default/spamassassin && . /etc/default/spamassassin
 
 test "$ENABLED" != "0" || exit 0
 
@@ -25,26 +30,28 @@ set -e
 case "$1" in
   start)
 	echo -n "Starting $DESC: "
-	start-stop-daemon --start --quiet --name $PNAME \
-		--oknodo --startas $DAEMON -- -d $OPTIONS
+	start-stop-daemon --start --pidfile $PIDFILE --name $PNAME \
+	    --startas $DAEMON -- $OPTIONS $DOPTIONS
 
 	echo "$NAME."
 	;;
   stop)
 	echo -n "Stopping $DESC: "
-	start-stop-daemon --stop --quiet --oknodo --name $PNAME
+
+	start-stop-daemon --stop --pidfile $PIDFILE --name $PNAME
+
 	echo "$NAME."
 	;;
   restart|force-reload)
 	echo -n "Restarting $DESC: "
-	start-stop-daemon --stop --quiet --name $PNAME --oknodo
-	sleep 1
-	start-stop-daemon --start --quiet --name $PNAME \
-		--oknodo --startas $DAEMON -- -d $OPTIONS
+	start-stop-daemon --stop --pidfile $PIDFILE --name $PNAME --retry 5
+	start-stop-daemon --start --pidfile $PIDFILE --name $PNAME \
+	    --startas $DAEMON -- $OPTIONS $DOPTIONS
+
 	echo "$NAME."
 	;;
   *)
-	N=/etc/init.d/$NAME
+	N=/etc/init.d/$SNAME
 	echo "Usage: $N {start|stop|restart|force-reload}" >&2
 	exit 1
 	;;
