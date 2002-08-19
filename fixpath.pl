ff7f4a9c07f22ba805223a2f280a7f1696846a7e
jmason <jmason@apache.org>
2002-08-19 17:06:38 +0000

    working on getting Makefile.PL to support non-root installers better
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@3064 13f79535-47bb-0310-9956-ffa450edef68

--- fixpath.pl
+++ fixpath.pl
@@ -1,9 +1,22 @@
 #!/usr/bin/perl
 
+my %defines = ();
+my $target;
+my $infile;
+while ($#ARGV >= 2) {
+  $_ = shift @ARGV;
+  if (/^-D([^\s\=]+)\=(.*)$/) { $defines{$1} = $2; next; }
+
+  last;
+}
+
+$infile = $ARGV[0];
+$target = $ARGV[1];
+
 use Config;
 my $perl = $Config{'perlpath'};
-my $target = pop @ARGV;
 
+open (IN, "<$infile") or die "cannot read $infile\n";
 open (OUT, ">$target") or die "cannot write to $target\n";
 
 # If we're using a CVS build, add the -w switch to turn on warnings
@@ -12,10 +25,12 @@ if (-f 'CVS/Entries') {
   $minusw = ' -w';
 }
 
-while (<>) {
+while (<IN>) {
   s,^\#!/usr/bin/perl[-Tw\s]*$,\#!${perl}${minusw}\n,g;
   s,^.*REMOVEFORINST.*$,,g;
+  s,\@\@([A-Z]\w+)\@\@,$defines{$1},gs;
   print OUT;
 }
 
+close IN;
 close OUT or die "cannot write to $target\n";
