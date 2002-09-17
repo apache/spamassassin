1d44a8098771558ff022c2073486ffc7ff57305e
jmason <jmason@unknown>
2002-09-17 10:56:24 +0000

    bug 893: rounding errors where hits=5.000....1 caused problems, fixed by Theo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/branches/b2_4_0@3433 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -166,10 +166,14 @@ sub check {
 
   $self->delete_fulltext_tmpfile();
 
+  # Round the hits to 3 decimal places to avoid rounding issues
+  # We assume required_hits to be properly rounded already.
+  $self->{hits} = sprintf "%0.3f", $self->{hits};
+  
   dbg ("is spam? score=".$self->{hits}.
-  			" required=".$self->{conf}->{required_hits}.
+                        " required=".$self->{conf}->{required_hits}.
                         " tests=".$self->get_names_of_tests_hit());
-  $self->{is_spam} = ($self->{hits} >= $self->{conf}->{required_hits});
+  $self->{is_spam} = $self->is_spam();
 
   if ($self->{conf}->{use_terse_report}) {
     $_ = $self->{conf}->{terse_report_template};
