d81ad181715e4c5c91e63a6e66228e0e80ebfb18
jmason <jmason@apache.org>
2002-12-03 00:07:44 +0000

    fix bug 1242: utf8 crud warnings in bayes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@3916 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Bayes.pm
+++ lib/Mail/SpamAssassin/Bayes.pm
@@ -451,9 +451,12 @@ sub tokenize {
 
 sub tokenize_line {
   my $self = $_[0];
-  local ($_) = $_[1];
   my $tokprefix = $_[2];
   my $isbody = $_[3];
+  local ($_);
+
+  # turn off utf8-ness to fix buginess
+  $_ = pack("C0A*", $_[1]);
 
   # include quotes, .'s and -'s for URIs, and [$,]'s for Nigerian-scam strings,
   # and ISO-8859-15 alphas.  Do not split on @'s; better results keeping it.
