22cff0e9eb1b8ab327d8e43e65cc9eb7a769c1ed
Theo Van Dinter <felicity@apache.org>
2002-12-10 21:29:21 +0000

    Turns out my understanding of the internal Razor2 object data is less than 100%.  This should finally handle different mime part cf correctly. <grrr>
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@3970 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/EvalTests.pm
+++ lib/Mail/SpamAssassin/EvalTests.pm
@@ -2524,7 +2524,7 @@ sub check_razor2_range {
     $self->razor2_lookup (\$full);
   }
 
-  return 1 if ( defined $self->{razor2_cf_score} && $self->{razor2_cf_score} >= $min && $self->{razor2_cf_score} <= $max );
+  return 1 if ( $self->{razor2_cf_score} >= $min && $self->{razor2_cf_score} <= $max );
   return 0;
 }
 
