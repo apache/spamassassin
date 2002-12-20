438b3fef773e93c96d912dd6c4e73b60f35b3322
jmason <jmason@apache.org>
2002-12-20 16:49:25 +0000

    added test for bug 1211
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4089 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Conf.pm
+++ lib/Mail/SpamAssassin/Conf.pm
@@ -1834,7 +1834,7 @@ sub add_to_addrlist {
   foreach my $addr (@addrs) {
     my $re = lc $addr;
     $re =~ s/[\000\\\(]/_/gs;			# paranoia
-    $re =~ s/([^\*\?_a-zA-Z0-9])/\\$1/g;		# escape any possible metachars
+    $re =~ s/([^\*\?_a-zA-Z0-9])/\\$1/g;	# escape any possible metachars
     $re =~ s/\?/\./g;                           # "?" -> "."
     $re =~ s/\*/\.\*/g;				# "*" -> "any string"
     $self->{$singlelist}->{$addr} = qr/^${re}$/;
