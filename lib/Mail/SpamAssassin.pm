69b44b5599e75f2f3490d2bf4b23549c62b16e1c
jmason <jmason@apache.org>
2002-12-21 19:53:42 +0000

    oops, Theo's right, already fixed
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4107 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -61,7 +61,6 @@ use 5.005;
 use Mail::SpamAssassin::Conf;
 use Mail::SpamAssassin::ConfSourceSQL;
 use Mail::SpamAssassin::PerMsgStatus;
-use Mail::SpamAssassin::PerMsgLearner;
 use Mail::SpamAssassin::NoMailAudit;
 use Mail::SpamAssassin::Bayes;
 
@@ -96,7 +95,7 @@ $TIMELOG->{dummy}=0;
 @ISA = qw();
 
 # SUB_VERSION is now <revision>-<yyyy>-<mm>-<dd>-<state>
-$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.148 2002/12/21 13:24:48 jmason Exp $'))[2 .. 5, 8]));
+$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.149 2002/12/21 19:53:42 jmason Exp $'))[2 .. 5, 8]));
 
 # If you hacked up your SA, add a token to identify it here. Eg.: I use
 # "mss<number>", <number> increasing with every hack.
