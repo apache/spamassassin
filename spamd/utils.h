b93b007da10fee7b2d6ae5dd5d01f57b110a045e
jmason <jmason@apache.org>
2003-01-23 00:58:27 +0000

    bug 1212: spamc -t timeout code from eugene AT invision.net
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4397 13f79535-47bb-0310-9956-ffa450edef68

--- spamd/utils.h
+++ spamd/utils.h
@@ -1,6 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+extern int libspamc_timeout;  /* default timeout in seconds */
+
+ssize_t timeout_read(ssize_t (*reader)(int d, void *buf, size_t nbytes), 
+                     int, void *, size_t );  
+
 int full_read(int fd, unsigned char *buf, int min, int len);
 int full_write(int fd, const unsigned char *buf, int len);
 
