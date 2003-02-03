cc2f575bfe6e3fbcf910cfafdfb3a9cd39104d84
Daniel Quinlan <quinlan@apache.org>
2003-02-03 07:23:52 +0000

    replace Win32 mkdir-based locking with sysopen-based locking add some debugging, comments, and warnings to locking code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4464 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Util.pm
+++ lib/Mail/SpamAssassin/Util.pm
@@ -2,6 +2,14 @@
 # functions that stand alone, without requiring a $self object,
 # Portability functions especially.
 
+# Copyright (C) 2003  Justin Mason
+# Copyright (C) 2003  Daniel Quinlan
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either the Artistic License or the GNU General
+# Public License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+
 package Mail::SpamAssassin::Util;
 
 use strict;
