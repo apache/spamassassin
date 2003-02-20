a5450ca2629345f037595d8629aff7ef3d3e6271
jmason <jmason@unknown>
2003-02-20 12:23:25 +0000

    changed suggested username length limit to 100 chars in SQL setup, so that virtual domains can work easily
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4604 13f79535-47bb-0310-9956-ffa450edef68

--- sql/spamassassin.sql
+++ sql/spamassassin.sql
@@ -1,5 +1,5 @@
 CREATE TABLE userpref (
-  username varchar(8) NOT NULL,
+  username varchar(100) NOT NULL,
   preference varchar(30) NOT NULL,
   value varchar(100) NOT NULL,
   prefid int(11) NOT NULL auto_increment,
