d6a5aa52e9334da160d5ce03cdbb3c3d44c3233c
Theo Van Dinter <felicity@apache.org>
2003-02-28 22:22:16 +0000

    test rules to look for RFC2822 "# of header" violations, also look for multiple Content-Type and Content-Transfer-Encoding headers which shouldn't exist in ham.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@4669 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/EvalTests.pm
+++ lib/Mail/SpamAssassin/EvalTests.pm
@@ -2556,4 +2556,11 @@ sub check_messageid_not_usable {
   return 0;
 }
 
+# Return true if the count of $hdr headers are within the given range
+sub check_header_count_range {
+  my ($self, $hdr, $min, $max) = @_;
+  my @hdrs = $self->{msg}->get_header ($hdr);
+  return ( scalar @hdrs >= $min && scalar @hdrs <= $max );
+}
+
 1;
