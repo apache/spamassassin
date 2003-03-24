1a76d572891e0e8e22ddca961e4d61f2ccdaa665
msquadrat <msquadrat@unknown>
2003-03-24 22:17:59 +0000

    bug 1656: updated init script for NetBSD
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/branches/b2_5_0@4829 13f79535-47bb-0310-9956-ffa450edef68

--- spamd/netbsd-rc-script.sh
+++ spamd/netbsd-rc-script.sh
@@ -1,26 +1,27 @@
-#! /bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
 # $NetBSD$
 #
 # The 'spamd' daemon checks emails provided by the 'spamc' client for signs
 # of spam
 #
-# PLEASE read the file @PREFIX@/share/doc/spamassassin/spamd/README.spamd,
+# PLEASE read the file
+#   @PREFIX@/share/doc/spamassassin/spamd/README.spamd
 # especially the section about security.
 
+## only for NetBSD
 # PROVIDE: spamd
 # REQUIRE: LOGIN
 # BEFORE: mail
+# KEYWORD: shutdown
+##
 
-PATH=/sbin:/usr/sbin:/bin:/usr/bin:@PREFIX@/sbin:@PREFIX@/bin
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:@PREFIX@/bin
 export PATH
 
 if [ -f /etc/rc.subr ]
 then
 	. /etc/rc.subr
-else
-	echo "$0: /etc/rc.subr is missing"
-	exit 1
 fi
 
 name="spamd"
@@ -31,20 +32,12 @@ pidfile="/var/run/${name}.pid"
 sig_stop="TERM"
 command_args="-d -r ${pidfile}"
 
-# default values, may be overridden by setting them in /etc/rc.conf
-spamd_flags="-H -c -a"
-spamd=NO
+# default values, may be overridden on NetBSD by setting them in /etc/rc.conf
+spamd_flags=${spamd_flags-"-H -c -a"}
+spamd=${spamd:-NO}
 
-INTERPRETER_SUPPORT=@INTERPRETER_SUPPORT@
-
-if checkyesno INTERPRETER_SUPPORT; then
-  : # support for 'command_interpreter' was added in NetBSD 1.6
-else
-  start_cmd="spamd_start"
-  stop_cmd="spamd_stop"
-  status_cmd="spamd_status"
-  the_spamd_pid=`check_pidfile ${pidfile} ${command_interpreter}`
-fi
+OPSYS=@OPSYS@ # set during package build
+INTERPRETER_SUPPORT=@INTERPRETER_SUPPORT@ # set during package build
 
 spamd_start()
 {
@@ -77,5 +70,45 @@ spamd_status()
 	
 }
 
-load_rc_config $name
-run_rc_command "$1"
+if [ "${OPSYS}" = "NetBSD" ]; then
+	if checkyesno INTERPRETER_SUPPORT; then
+	  : # support for 'command_interpreter' was added in NetBSD 1.6
+	else
+	  start_cmd="spamd_start"
+	  stop_cmd="spamd_stop"
+	  status_cmd="spamd_status"
+	  the_spamd_pid=`check_pidfile ${pidfile} ${command_interpreter}`
+	fi
+
+	load_rc_config $name
+	run_rc_command "$1"
+
+else # not NetBSD
+
+	if [ -f ${pidfile} ];  then
+		the_spamd_pid=`head -1 ${pidfile}`
+	else
+		the_spamd_pid=
+	fi
+
+	case ${1+"$@"} in
+	start)
+		spamd_start
+		;;
+	stop)
+		spamd_stop
+		;;
+	restart)
+		spamd_stop
+		sleep 1
+		spamd_start
+		;;
+	status)
+		spamd_status
+		;;
+	*) 
+		echo "Usage: ${0} (start|stop|restart|status)"
+		;;
+
+	esac
+fi
