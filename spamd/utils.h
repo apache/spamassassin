8bc9838f026fbd9571a7971fa64196a962e7047c
jmason <jmason@unknown>
2003-03-26 20:01:29 +0000

    bug 1637: compilation of spamd/sslspamc broken, fixed
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/branches/b2_5_0@4849 13f79535-47bb-0310-9956-ffa450edef68

--- spamd/utils.h
+++ spamd/utils.h
@@ -3,9 +3,21 @@
 
 extern int libspamc_timeout;  /* default timeout in seconds */
 
-ssize_t timeout_read(ssize_t (*reader)(int d, void *buf, size_t nbytes), 
-                     int, void *, size_t );  
+#ifdef SPAMC_SSL
+#include <openssl/crypto.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#else
+typedef int	SSL;	/* fake type to avoid conditional compilation */
+typedef int	SSL_CTX;
+typedef int	SSL_METHOD;
+#endif
 
+ssize_t fd_timeout_read (int fd, void *, size_t );  
+int ssl_timeout_read (SSL *ssl, void *, int );  
+
+/* these are fd-only, no SSL support */
 int full_read(int fd, unsigned char *buf, int min, int len);
 int full_write(int fd, const unsigned char *buf, int len);
 
