e3e6723c737a651fc942602a223e9c795427d7eb
Daniel Quinlan <quinlan@apache.org>
2003-08-26 04:03:45 +0000

    bug 2338: need to reseed PRNG for each new child
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@5713 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -94,7 +94,7 @@ $TIMELOG->{dummy}=0;
 @ISA = qw();
 
 # SUB_VERSION is now <revision>-<yyyy>-<mm>-<dd>-<state>
-$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.198 2003/08/22 23:28:06 felicity Exp $'))[2 .. 5, 8]));
+$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.199 2003/08/26 04:03:45 quinlan Exp $'))[2 .. 5, 8]));
 
 # If you hacked up your SA, add a token to identify it here. Eg.: I use
 # "mss<number>", <number> increasing with every hack.
@@ -1167,8 +1167,16 @@ sub lint_rules {
 sub init {
   my ($self, $use_user_pref) = @_;
 
-  if ($self->{_initted}) { return; }
-  $self->{_initted} = 1;
+  if (defined $self->{_initted}) {
+    # seed PRNG whenever the process id changes
+    if ($self->{_initted} != $$) {
+      $self->{_initted} = $$;
+      srand;
+    }
+    return;
+  }
+
+  $self->{_initted} = $$;
 
   #fix spamd reading root prefs file
   unless (defined $use_user_pref) {
