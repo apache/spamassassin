2dda47cc1099ace917dfd50b1d595854c15713e6
Theo Van Dinter <felicity@apache.org>
2003-09-16 02:33:19 +0000

    re-do devel version
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@5777 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -86,7 +86,7 @@ use vars qw{
 };
 
 $VERSION = "2.60";              # update after release
-#$IS_DEVEL_BUILD = 1;            # change for release versions
+$IS_DEVEL_BUILD = 1;            # change for release versions
 
 # Create the hash so that it really points to something, otherwise we can't
 # get a reference to it -- Marc
@@ -94,7 +94,7 @@ $TIMELOG->{dummy}=0;
 @ISA = qw();
 
 # SUB_VERSION is now <revision>-<yyyy>-<mm>-<dd>-<state>
-$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.204 2003/09/16 02:32:57 felicity Exp $'))[2 .. 5, 8]));
+$SUB_VERSION = lc(join('-', (split(/[ \/]/, '$Id: SpamAssassin.pm,v 1.205 2003/09/16 02:33:19 felicity Exp $'))[2 .. 5, 8]));
 
 # If you hacked up your SA, add a token to identify it here. Eg.: I use
 # "mss<number>", <number> increasing with every hack.
