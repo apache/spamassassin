f5cc1a929bbf19aa4bbd46d71f2f59913315c321
Theo Van Dinter <felicity@apache.org>
2004-01-08 23:50:40 +0000

    bug 2851: "spamassassin -r" (aka M::SA::report_as_spam()) wouldn't remove SA markup for learning like it did for reporting.  add in a remove_spamassassin_markup() call before anything else in report_as_spam().
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/branches/b2_6_0@6124 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -633,6 +633,10 @@ sub report_as_spam {
 
   $self->init(1);
 
+  # Let's make sure the markup was removed first ...
+  my @msg = split (/^/m, $self->remove_spamassassin_markup($mail));
+  $mail = Mail::SpamAssassin::NoMailAudit->new ('data' => \@msg);
+
   $mail = $self->encapsulate_mail_object ($mail);
 
   # learn as spam if enabled
@@ -674,6 +678,10 @@ sub revoke_as_spam {
   local ($_);
 
   $self->init(1);
+
+  # Let's make sure the markup was removed first ...
+  my @msg = split (/^/m, $self->remove_spamassassin_markup($mail));
+  $mail = Mail::SpamAssassin::NoMailAudit->new ('data' => \@msg);
 
   $mail = $self->encapsulate_mail_object ($mail);
 
