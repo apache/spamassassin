fe2dc2005002c9eaa3aff8035ab1347787e49d1d
Malte Sebastian Stretz <mss@apache.org>
2004-01-24 20:05:23 +0000

    Added some license/copyright headers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@6262 13f79535-47bb-0310-9956-ffa450edef68

--- spamc/configure.pl
+++ spamc/configure.pl
@@ -1,7 +1,24 @@
 #!/usr/bin/perl
 # autoconf wrapper (for Unix)/alternative (for Windows)
 #
-# Copyright (c) 2004 Malte S. Stretz <spamassassin-contrib (at) msquadrat.de>
+# Copyright 2004 by Malte S. Stretz.
+# Based on work done by Sidney Markowitz.
+#
+# <@LICENSE>
+# Copyright 2004 Apache Software Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# </@LICENSE>
 
 
 use strict;
