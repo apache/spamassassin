3bab35485296291dd435cf20dbbd36e9575c7e35
Michael Parker <parker@apache.org>
2004-03-01 05:09:39 +0000

    PR2457: Updated userpref create def, also re added the PHP admin stuff with a corrected URL
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@6942 13f79535-47bb-0310-9956-ffa450edef68

--- sql/userpref_mysql.sql
+++ sql/userpref_mysql.sql
@@ -1,9 +1,8 @@
 CREATE TABLE userpref (
-  username varchar(100) NOT NULL,
-  preference varchar(30) NOT NULL,
-  value varchar(100) NOT NULL,
+  username varchar(100) NOT NULL default '',
+  preference varchar(30) NOT NULL default '',
+  value varchar(100) NOT NULL default '',
   prefid int(11) NOT NULL auto_increment,
-  PRIMARY KEY (prefid),
-  INDEX (username)
+  PRIMARY KEY  (prefid),
+  KEY username (username)
 ) TYPE=MyISAM;
-
