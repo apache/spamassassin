873dc47f5e7a17b185aae3a74ce638d565594c49
Michael Parker <parker@apache.org>
2004-03-01 04:07:29 +0000

    PR3095: Update sql documentation to include PostgreSQL examples and instructions, parts from Feargal Reilly
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@6941 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sql/userpref_pg.sql
@@ -0,0 +1,7 @@
+CREATE TABLE userpref (
+  prefid bigserial NOT NULL unique primary key,
+  username varchar(100) NOT NULL,
+  preference varchar(30) NOT NULL,
+  value varchar(100) NOT NULL
+);
+CREATE INDEX userpref_username_idx ON userpref(prefid);
