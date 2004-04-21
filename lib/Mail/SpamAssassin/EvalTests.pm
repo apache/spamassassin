5d2953f3ce7a31f0526174db14e13a200d1b8693
Daniel Quinlan <quinlan@apache.org>
2004-04-21 01:08:19 +0000

    fixes for T_REDIRECTOR
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@10141 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/EvalTests.pm
+++ lib/Mail/SpamAssassin/EvalTests.pm
@@ -3267,13 +3267,10 @@ sub check_for_http_redirector {
   my ($self) = @_;
 
   foreach ($self->get_uri_list()) {
-    while (s{^https?://([^/:\?]+).+?(https?://([^/:\?]+).+)$}{$2}g) {
-      my ($redir, $dest) = ($1,$3);
-      foreach ($redir,$dest) {
-	# Strip down to domain.tld
-        s/^(?:.+\.)([^.]+\.[^.]+)$/$1/;
-	# make sure we do things case-insensitively
-	$_ = lc $_;
+    while (s{^https?://([^/:\?]+).+?(https?://([^/:\?]+).*)$}{$2}) {
+      my ($redir, $dest) = ($1, $3);
+      foreach ($redir, $dest) {
+	$_ = Mail::SpamAssassin::Util::uri_to_domain(lc($_));
       }
       next if ($redir eq $dest);
       dbg("redirect: found $redir to $dest, flagging");
