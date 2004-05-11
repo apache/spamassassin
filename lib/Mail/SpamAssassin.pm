2cf952abd049945b1165a80fdfd475374494a853
Justin Mason <jm@apache.org>
2004-05-11 07:19:44 +0000

    added a diagnostic method to --lint, which lists the versions of various installed modules for debugging
    git-svn-id: https://svn.apache.org/repos/asf/incubator/spamassassin/trunk@10591 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -1140,6 +1140,35 @@ sub compile_now {
 
 ###########################################################################
 
+=item $f->debug_diagnostics ()
+
+Output some diagnostic information, useful for debugging SpamAssassin
+problems.
+
+=cut
+
+sub debug_diagnostics {
+  my ($self) = @_;
+
+  foreach my $module (sort qw(
+        Net::DNS Razor2::Client::Agent MIME::Base64
+        IO::Socket::UNIX DB_File Digest::SHA1
+        MIME::Base64 DBI URI Net::LDAP Storable
+        ))
+  {
+    my $modver;
+    if (eval ' require '.$module.'; $modver = $'.$module.'::VERSION; 1;')
+    {
+      $modver ||= '(undef)';
+      dbg ("diag: module installed: $module, version $modver");
+    } else {
+      dbg ("diag: module not installed: $module ('require' failed)");
+    }
+  }
+}
+
+###########################################################################
+
 =item $failed = $f->lint_rules ()
 
 Syntax-check the current set of rules.  Returns the number of 
