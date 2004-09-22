90b485766d8ada0e2881e2c9bd8fac61d22d1326
Theo Van Dinter <felicity@apache.org>
2004-09-22 02:25:06 +0000

    prep 3.0.1 devel cycle
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.0@47021 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -98,8 +98,8 @@ use vars qw{
   @site_rules_path
 };
 
-$VERSION = "3.000000";      # update after release (same format as perl $])
-$IS_DEVEL_BUILD = 0;        # change for release versions
+$VERSION = "3.000001";      # update after release (same format as perl $])
+$IS_DEVEL_BUILD = 1;        # change for release versions
 
 @ISA = qw();
 
