147a1dfee1df47c924bd31e6d0f4587cb4faf58c
Theo Van Dinter <felicity@apache.org>
2004-10-01 19:23:57 +0000

    bug 3854: _TESTSSCORES_ didn't return 'none' if no rules hit, just a blank scalar.
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@51750 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -1148,7 +1148,7 @@ sub _get_tag {
                   $line .= $arg . $test . "=" . $self->{conf}->{scores}->{$test};
                 }
               }
-              return $line;
+              return $line ? $line : 'none';
             },
 
             PREVIEW => sub { $self->get_content_preview() },
