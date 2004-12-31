fe7ae797eb4c89200ed8539ea4f725ff1356addf
Malte Sebastian Stretz <mss@apache.org>
2004-12-31 00:59:56 +0000

    bug 4053:  Jason J Ellingson reported that beginning with version 1.21 XMail will put square brackets around the IP in the Received header.
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@123754 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Message/Metadata/Received.pm
+++ lib/Mail/SpamAssassin/Message/Metadata/Received.pm
@@ -435,22 +435,10 @@ sub parse_received_line {
       }
     }
 
-    if (/\[XMail /) { # bug 3791
-      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org> from <bounce-cscommunity-11965901@list.cramsession.com>; Sat, 18 Sep 2004 23:17:54 -0500
-      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net (209.32.147.34:25) with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org> from <bounce-cscommunity-11965901@list.cramsession.com>; Sat, 18 Sep 2004 23:17:54 -0500
-      if (/^from (\S+) \((${IP_ADDRESS})(?::\d+)?\) by (\S+)(?: \(\S+\)|) with \[XMail/)
-      {
-	$helo = $1; $ip = $2; $by = $3;
-        / id <(\S+)> / and $id = $1;
-        / from <(\S+)>; / and $envfrom = $1;
-        goto enough;
-      }
-    }
-
-    if (/\[XMail /) { # bug 3791
-      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org> from <bounce-cscommunity-11965901@list.cramsession.com>; Sat, 18 Sep 2004 23:17:54 -0500
-      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net (209.32.147.34:25) with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org> from <bounce-cscommunity-11965901@list.cramsession.com>; Sat, 18 Sep 2004 23:17:54 -0500
-      if (/^from (\S+) \((${IP_ADDRESS})(?::\d+|)\) by (\S+)(?: \(\S+\)|) with \[XMail/)
+    if (/\[XMail /) { # bug 3791, bug 4053
+      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org.spamassassin.org> from <bounce-cscommunity-11965901@list.cramsession.com.spamassassin.org>; Sat, 18 Sep 2004 23:17:54 -0500
+      # Received: from list.brainbuzz.com (63.146.189.86:23198) by mx1.yourtech.net (209.32.147.34:25) with [XMail 1.20 ESMTP Server] id <S72E> for <jason@ellingson.org.spamassassin.org> from <bounce-cscommunity-11965901@list.cramsession.com.spamassassin.org>; Sat, 18 Sep 2004 23:17:54 -0500
+      if (/^from (\S+) \((\[?${IP_ADDRESS}\]?)(?::\d+|)\) by (\S+)(?: \(\S+\)|) with \[XMail/)
       {
 	$helo = $1; $ip = $2; $by = $3;
         / id <(\S+)> / and $id = $1;
