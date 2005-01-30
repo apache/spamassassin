9b890854bea306115a6e941c51fb3bb113aff114
Daniel Quinlan <quinlan@apache.org>
2005-01-30 22:56:06 +0000

    make sure every plugin has NAME, SYNOPSIS, DESCRIPTION, and a license block at the top of the file
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@149177 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/Test.pm
+++ lib/Mail/SpamAssassin/Plugin/Test.pm
@@ -1,9 +1,32 @@
-=head1 
+# <@LICENSE>
+# Copyright 2004 Apache Software Foundation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# </@LICENSE>
 
-To try this out, write these lines to /etc/mail/spamassassin/plugintest.cf:
+=head1 NAME
+
+Test - test plugin
+
+=head1 SYNOPSIS
 
   loadplugin     Mail::SpamAssassin::Plugin::Test
   header         MY_TEST_PLUGIN eval:check_test_plugin()
+
+=head1 DESCRIPTION
+
+To try this plugin, write the above two lines in the synopsis to
+C</etc/mail/spamassassin/plugintest.cf>.
 
 =cut
 
