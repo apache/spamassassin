155fb5240ce00097d3a238709d7a71e66598db89
Daniel Quinlan <quinlan@apache.org>
2005-03-12 00:41:34 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@157193 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Conf.pm
+++ lib/Mail/SpamAssassin/Conf.pm
@@ -987,8 +987,6 @@ Select the locales to allow from the list below:
     type => $CONF_TYPE_STRING
   });
 
-=back
-
 =head2 NETWORK TEST OPTIONS
 
 =item trusted_networks ip.add.re.ss[/mask] ...   (default: none)
