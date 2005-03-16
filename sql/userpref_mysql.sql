ea322a0aee314e2c6fb97915ab98b64f3c4656b1
Michael Parker <parker@apache.org>
2005-03-16 20:59:15 +0000

    Bug 3904: Increase size of preference column
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@157800 13f79535-47bb-0310-9956-ffa450edef68

--- sql/userpref_mysql.sql
+++ sql/userpref_mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE userpref (
   username varchar(100) NOT NULL default '',
-  preference varchar(30) NOT NULL default '',
+  preference varchar(50) NOT NULL default '',
   value varchar(100) NOT NULL default '',
   prefid int(11) NOT NULL auto_increment,
   PRIMARY KEY  (prefid),
