6e1ccdbaa789714438698dad3df8c1bfe4802877
Michael Parker <parker@apache.org>
2005-03-23 07:16:24 +0000

    An initial cut at a pure perl client for the spamd protocol.
    Right now it just implements process, check, learn and ping.
    
    We can add the others as time goes by.
    
    I'm also not set on the current API, so I expect it to change
    a bit.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@158743 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lib/Mail/SpamAssassin/Client.pm
@@ -0,0 +1,357 @@
+# NOTE: This interface is alpha at best, and almost guaranteed to change
+# <@LICENSE>
+# Copyright 2004 Apache Software Foundation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# </@LICENSE>
+
+=head1 NAME
+
+Mail::SpamAssassin::Client - Client for spamd Protocol
+
+NOTE: This interface is alpha at best, and almost guaranteed to change
+
+=head1 SYNOPSIS
+
+  my $client = new Mail::SpamAssassin::Client({port => 783,
+                                               host => 'localhost',
+                                               username => 'someuser'});
+
+  if ($client->ping()) {
+    print "Ping is ok\n";
+  }
+
+  my $result = $client->process($testmsg);
+
+  if ($result->{isspam} eq 'True') {
+    do something with spam message here
+  }
+
+=head1 DESCRIPTION
+
+Mail::SpamAssassin::Client is a module that provides a perl implementation for
+the spamd protocol.
+
+=cut
+
+package Mail::SpamAssassin::Client;
+
+use IO::Socket;
+
+my $EOL = "\015\012";
+my $BLANK = $EOL x 2;
+my $PROTOVERSION = 'SPAMC/1.3';
+
+=head1 PUBLIC METHODS
+
+=head2 new
+
+public class (Mail::SpamAssassin::Client) new (\% $args)
+
+Description:
+This method creates a new Mail::SpamAssassin::Client object.
+
+=cut
+
+sub new {
+  my ($class, $args) = @_;
+
+  $class = ref($class) || $class;
+
+  my $self = {};
+
+  # with a sockets_path set then it makes no sense to set host and port
+  if ($args->{socketpath}) {
+    $self->{socketpath} = $args->{socketpath};
+  }
+  else {
+    $self->{port} = $args->{port};
+    $self->{host} = $args->{host};
+  }
+
+  if ($args->{username}) {
+    $self->{username} = $args->{username};
+  }
+
+  bless($self, $class);
+
+  $self;
+}
+
+=head2 process
+
+public instance (\%) process (String $msg, Boolean $is_check_p)
+
+Description:
+This method makes a call to the spamd server and depending on the value of
+C<$is_check_p> either calls PROCESS or CHECK.
+
+The return value is a hash reference containing several pieces of information,
+if available:
+
+content_length
+
+isspam
+
+score
+
+threshold
+
+message
+
+=cut
+
+sub process {
+  my ($self, $msg, $is_check_p) = @_;
+
+  my %data;
+
+  my $command = $is_check_p ? 'CHECK' : 'PROCESS';
+
+  $self->clear_errors();
+
+  my $remote = $self->_create_connection();
+
+  return 0 unless ($remote);
+
+  my $msgsize = length($msg.$EOL);
+
+  print $remote "$command $PROTOVERSION$EOL";
+  print $remote "Content-length: $msgsize$EOL";
+  print $remote "User: $self->{username}$EOL" if ($self->{username});
+  print $remote "$EOL";
+  print $remote $msg;
+  print $remote "$EOL";
+
+  my $line = <$remote>;
+  my ($version, $resp_code, $resp_msg) = $self->_parse_response_line($line);
+
+  $self->{resp_code} = $resp_code;
+  $self->{resp_msg} = $resp_msg;
+
+  return undef unless ($resp_code == 0);
+
+  my $found_blank_line_p = 0;
+
+  while (!$found_blank_line_p) {
+    $line = <$remote>;
+
+    if ($line =~ /Content-length: (\d+)/) {
+      $data{content_length} = $1;
+    }
+    elsif ($line =~ m!Spam: (\S+) ; (\S+) / (\S+)!) {
+      $data{isspam} = $1;
+      $data{score} = $2 + 0;
+      $data{threshold} = $3 + 0;
+    }
+    elsif ($line =~ /$EOL/) {
+      $found_blank_line_p = 1;
+      print STDERR "Found the blank line\n";
+    }
+  }
+
+  my $return_msg;
+  while(<$remote>) {
+    $return_msg .= $_;
+  }
+
+  $data{message} = $return_msg if ($return_msg);
+
+  close $remote;
+
+  return \%data;
+}
+
+=head2 check
+
+public instance (\%) check (String $msg)
+
+Description:
+The method implements the check call.
+
+Since check and process are so similar, we simply pass this
+call along to the process method with a flag to indicate
+to actually make the CHECK call.
+
+See the process method for the return value.
+
+=cut
+
+sub check {
+  my ($self, $msg) = @_;
+
+  return $self->process($msg, 1);
+}
+
+=head2 learn
+
+public instance (Boolean) learn (String $msg, Integer $learntype)
+
+Description:
+This method implements the learn call.  C<$learntype> should be
+an integer, 0 for spam, 1 for ham and 2 for forget.  The return
+value is a boolean indicating if the message was learned or not.
+
+An undef return value indicates that there was an error and you
+should check the resp_code/resp_error values to determine what
+the error was.
+
+=cut
+
+sub learn {
+  my ($self, $msg, $learntype) = @_;
+
+  $self->clear_errors();
+
+  my $remote = $self->_create_connection();
+
+  return undef unless ($remote);
+
+  my $msgsize = length($msg.$EOL);
+
+  print $remote "LEARN $PROTOVERSION$EOL";
+  print $remote "Content-length: $msgsize$EOL";
+  print $remote "User: $self->{username}$EOL" if ($self->{username});
+  print $remote "Learn-type: $learntype$EOL";
+  print $remote "$EOL";
+  print $remote $msg;
+  print $remote "$EOL";
+
+  my $line = <$remote>;
+
+  my ($version, $resp_code, $resp_msg) = $self->_parse_response_line($line);
+
+  $self->{resp_code} = $resp_code;
+  $self->{resp_msg} = $resp_msg;
+
+  return undef unless ($resp_code == 0);
+
+  my $learned_p = 0;
+  my $found_blank_line_p = 0;
+
+  while (!$found_blank_line_p) {
+    $line = <$remote>;
+
+    if ($line =~ /Learned: yes/i) {
+      $learned_p = 1;
+    }
+    elsif ($line =~ /Learned: no/i) {
+      $learned_p = 0;
+    }
+    elsif ($line =~ /$EOL/) {
+      $found_blank_line_p = 1;
+      print STDERR "Found the blank line\n";
+    }
+  }
+
+  close $remote;
+
+  return $learned_p;
+}
+
+=head2 ping
+
+public instance (Boolean) ping ()
+
+Description:
+This method performs a server ping and returns 0 or 1 depending on
+if the server responded correctly.
+
+=cut
+
+sub ping {
+  my ($self) = @_;
+
+  my $remote = $self->_create_connection();
+
+  return 0 unless ($remote);
+
+  print $remote "PING $PROTOVERSION$EOL";
+  print $remote "$EOL";
+
+  my $line = <$remote>;
+  close $remote;
+
+  my ($version, $resp_code, $resp_msg) = $self->_parse_response_line($line);
+
+  return 0 unless ($resp_msg eq 'PONG');
+
+  return 1;
+}
+
+=head1 PRIVATE METHODS
+
+=head2 _create_connection
+
+private instance (IO::Socket) _create_connection ()
+
+Description:
+This method sets up a proper IO::Socket connection based on the arguments
+used when greating the client object.
+
+On failure, it sets an internal error code and returns undef.
+
+=cut
+
+sub _create_connection {
+  my ($self) = @_;
+
+  my $remote;
+
+  if ($self->{socketpath}) {
+    $remote = IO::Socket::UNIX->new( Peer => $self->{socketpath},
+				     Type => SOCK_STREAM,
+				   );
+  }
+  else {
+    $remote = IO::Socket::INET->new( Proto     => "tcp",
+				     PeerAddr  => $self->{host},
+				     PeerPort  => $self->{port},
+				   );
+  }
+
+  unless ($remote) {
+    print "Failed to create connection to spamd daemon: $!\n";
+    return undef;
+  }
+
+  $remote;
+}
+
+=head2 _parse_response_line
+
+private instance (@) _parse_response_line (String $line)
+
+Description:
+This method parses the initial response line/header from the server
+and returns its parts.
+
+We have this as a seperate method in case we ever decide to get fancy
+with the response line.
+
+=cut
+
+sub _parse_response_line {
+  my ($self, $line) = @_;
+
+  return split(/\s+/, $line);
+}
+
+sub clear_errors {
+  my ($self) = @_;
+
+  $self->{resp_code} = undef;
+  $self->{resp_msg} = undef;
+}
+1;
+
