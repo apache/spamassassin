55be0fb4f6fcf1d9c2ee4903f88fa57b94060fa9
Justin Mason <jm@apache.org>
2006-02-02 20:49:57 +0000

    bug 4759: 'fetchmail marker' only valid in the trusted header set of Received lines
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.1@374490 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Message/Metadata/Received.pm
+++ lib/Mail/SpamAssassin/Message/Metadata/Received.pm
@@ -257,8 +257,10 @@ sub parse_received_headers {
 
     if ($in_trusted) {
       push (@{$self->{relays_trusted}}, $relay);
+      $self->{allow_fetchmail_markers} = 1;
     } else {
       push (@{$self->{relays_untrusted}}, $relay);
+      $self->{allow_fetchmail_markers} = 0;
     }
   }
 
@@ -1217,8 +1219,12 @@ sub make_relay_as_string {
 # spamcop does this, and it's a great idea ;)
 sub found_pop_fetcher_sig {
   my ($self) = @_;
-  dbg("received-header: found fetchmail marker, restarting parse");
-  $self->{relays} = [ ];
+  if ($self->{allow_fetchmail_markers}) {
+    dbg("received-header: found fetchmail marker, restarting parse");
+    $self->{relays_trusted} = [ ];
+  } else {
+    dbg("received-header: found fetchmail marker outside trusted area, ignored");
+  }
 }
 
 # ---------------------------------------------------------------------------
