1f47c0910845963ff0d5adafde084ea784ce0566
Justin Mason <jm@apache.org>
2006-07-09 16:53:31 +0000

    merging forward changes from trunk, again; 418363:420312, now up to 420312
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/bug-3109-shortcircuiting@420317 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -1089,6 +1089,7 @@ sub bayes_report_make_list {
 ###########################################################################
 
 # public API for plugins
+
 =item $status->set_tag($tagname, $value)
 
 Set a template tag, as used in C<add_header>, report templates, etc. This API
@@ -1121,6 +1122,7 @@ sub set_tag {
 }
 
 # public API for plugins
+
 =item $string = $status->get_tag($tagname)
 
 Get the current value of a template tag, as used in C<add_header>, report
