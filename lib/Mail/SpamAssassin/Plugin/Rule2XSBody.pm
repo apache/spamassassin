7922797b472a6433478f9ef536ab0c4e5245fc8e
Justin Mason <jm@apache.org>
2006-08-13 15:16:43 +0000

    extend re2xs to store the original regexps, too, so that callers can see exactly what REs are c-ified through introspection of a HAS_RULES hash. read the module name from the input file. make writing of re.in optional in plugin, driven by ENV-variable back channel. modify re2xs to automatically rm the output module tree if --force is used.  extend 'start_rules' plugin API to pass the priority, which was an omission.  support use of re2c module in scan.  add a quick driver script which builds the ruleset (for me anyway)
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/jm_re2c_hacks@431184 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/Rule2XSBody.pm
+++ lib/Mail/SpamAssassin/Plugin/Rule2XSBody.pm
@@ -19,8 +19,6 @@ package Mail::SpamAssassin::Plugin::Rule2XSBody;
 use Mail::SpamAssassin::Plugin;
 use Mail::SpamAssassin::Logger;
 
-my $re2xs_out = "re.in";
-
 use strict;
 use warnings;
 use bytes;
@@ -57,27 +55,115 @@ sub set_config {
 sub finish_parsing_end {
   my ($self, $params) = @_;
   my $conf = $params->{conf};
-  $self->zoomify_test_set ($conf->{body_tests}, 'body');
+
+  # TODO: need a better way to do this rather than using an env
+  # var as a back channel
+  if ($ENV{'RULE_REGEXP_DUMP_FILE'}) {
+    $conf->{env_regexp_dump_file} = $ENV{'RULE_REGEXP_DUMP_FILE'};
+  }
+
+  $self->zoomify_test_set ($conf, $conf->{body_tests}, 'body');
 }
 
 sub zoomify_test_set {
-  my ($self, $test_set, $ruletype) = @_;
+  my ($self, $conf, $test_set, $ruletype) = @_;
   foreach my $pri (keys %{$test_set}) {
     my $nicepri = $pri; $nicepri =~ s/-/neg/g;
-    $self->extract_all($test_set->{$pri}, $ruletype.'_'.$nicepri);
+    $self->check_all($conf, $test_set->{$pri}, $ruletype.'_'.$nicepri);
+
+    if ($conf->{env_regexp_dump_file}) {
+      $self->extract_all($conf, $test_set->{$pri}, $ruletype.'_'.$nicepri);
+    }
   }
 }
 
 ###########################################################################
 
+sub check_all {
+  my ($self, $conf, $rules, $ruletype) = @_;
+
+  my $modname = "Mail::SpamAssassin::CompiledRegexps::".$ruletype;
+  my $hasrules;
+
+  if (!eval qq{ use $modname; \$hasrules = \$${modname}::HAS_RULES; 1; }) {
+    # the module isn't available, so no rules will be either
+    return 0;
+  }
+
+  my $found = 0;
+  foreach my $name (keys %{$rules}) {
+    my $rule = $rules->{$name};
+    next unless ($hasrules->{$name} && $hasrules->{$name} eq $rule);
+
+    # we have the rule, and its regexp matches.  zero out the body
+    # rule, so that the module can do the work instead
+
+    # TODO: need a cleaner way to do this.  I expect when rule types
+    # are implementable in plugins, I can do it that way
+    $conf->{zoom_disable_basic_regexp_rule}->{$name} = 1;
+    $found++;
+  }
+
+  if ($found) {
+    $conf->{zoom_ruletypes_available} ||= { };
+    $conf->{zoom_ruletypes_available}->{$ruletype} = 1;
+    dbg("zoom: $found compiled rules are available for type $ruletype");
+  }
+}
+
+sub run_body_hack {
+  my ($self, $params) = @_;
+
+  return unless ($params->{ruletype} eq 'body');
+
+  my $nicepri = $params->{priority}; $nicepri =~ s/-/neg/g;
+  my $ruletype = ($params->{ruletype}.'_'.$nicepri);
+  my $scanner = $params->{permsgstatus};
+  my $conf = $scanner->{conf};
+  return unless $conf->{zoom_ruletypes_available}->{$ruletype};
+
+  # create a new ref to a hash copy of {zoom_disable_basic_regexp_rule};
+  # this will be given to the $scanner object below after our scan
+  my $skiprulesptr = \%{$conf->{zoom_disable_basic_regexp_rule}};
+  dbg("zoom: run_body_hack for $ruletype start");
+
+  my $scoresptr = $conf->{scores};
+  my $modname = "Mail::SpamAssassin::CompiledRegexps::".$ruletype;
+
+  foreach my $line (@{$params->{lines}}) {
+
+    # TODO: make rules case-sensitive, this lc operation is probably
+    # expensive!
+
+    no strict "refs";
+    my $results = &{$modname.'::scan'}(lc $line);
+    use strict "refs";
+
+    foreach my $rulename (@{$results}) {
+      next unless $scoresptr->{$rulename};
+      dbg("zoom: re2c form of $rulename fired");
+      # allow the rule to be run as a body rule, if the re2c form hit
+      $skiprulesptr->{$rulename} = 0;
+    }
+  }
+
+  dbg("zoom: run_body_hack for $ruletype done");
+  $scanner->{skip_body_rules} = $skiprulesptr;
+}
+
+###########################################################################
+
 sub extract_all {
-  my ($self, $rules, $ruletype) = @_;
+  my ($self, $conf, $rules, $ruletype) = @_;
   my $yes = 0;
   my $no = 0;
   my @good_bases = ();
   my @failed = ();
 
   dbg("zoom: base extraction start for type $ruletype");
+
+  $conf->{env_regexp_dump_file} =~ /^(.*)$/;
+  my $dumpfile = $1;
 
   foreach my $name (keys %{$rules}) {
     my $rule = $rules->{$name};
@@ -114,10 +200,16 @@ sub extract_all {
   # returned as two hits, correctly.  So we only have to be smart about the
   # full-subsumption case; overlapping is taken care of for us, by re2c.
 
+  open (OUT, ">$dumpfile") or die "cannot write to $dumpfile!";
+  print OUT "name $ruletype\n";
+
   my %newbases = ();
   foreach my $set1 (@good_bases) {
     my $base1 = $set1->{base};
+    my $orig1 = $set1->{orig};
     my $key1  = $set1->{name};
+
+    print OUT "orig $key1 $orig1\n";
 
     foreach my $set2 (@good_bases) {
       next if ($set1 == $set2);
@@ -133,13 +225,12 @@ sub extract_all {
     }
   }
 
-  open (OUT, ">$re2xs_out") or die "cannot write to $re2xs_out!";
   foreach my $set (@good_bases) {
     my $base = $set->{base};
     my $key  = $set->{name};
-    print OUT "$base:$key\n";
+    print OUT "r $base:$key\n";
   }
-  close OUT or die "close failed on $re2xs_out!";
+  close OUT or die "close failed on $dumpfile!";
 
   # TODO: run re2xs
 
@@ -164,9 +255,8 @@ sub extract_base {
   $rule = Mail::SpamAssassin::Util::regexp_remove_delimiters($rule);
 
   # remove those mods, keep for later
-  $rule =~ s/^(\(?[a-z]*\))//;
-  my $mods = $1;
-  $mods ||= '';
+  my $mods = '';
+  if ($rule =~ s/^(\(?[a-z]*\))//) { $mods = $1; }
 
   # now: simplify aspects of the regexp.  Bear in mind that we can
   # simplify as long as we cause the regexp to become more general;
@@ -188,7 +278,9 @@ sub extract_base {
   $rule =~ s/\\b//gs;
 
   # here's the trick; we can use the truncate regexp below simply by
-  # reversing the string and taking care to fix "\z" 2-char escapes
+  # reversing the string and taking care to fix "\z" 2-char escapes.
+  # TODO: this breaks stuff like "\s+" or "\S{4,12}", but since the
+  # truncation regexp below is pretty simple-minded, that's ok.
   if ($is_reversed) {
     $rule = join ('', reverse (split '', $rule));
     $rule =~ s/(.)\\/\\$1/gs;
@@ -227,6 +319,7 @@ sub extract_base {
 
   # finally, reassemble a usable regexp
   $rule = $mods . $rule;
+
   return $rule;
 }
 
