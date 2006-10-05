bc6a1acc916fa71f16aca8df6bee90e056be9508
Daryl C. W. O'Shea <dos@apache.org>
2006-10-05 17:59:08 +0000

    3.1.7 devel cycle started
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.1@453304 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -95,8 +95,8 @@ use vars qw{
   @site_rules_path
 };
 
-$VERSION = "3.001006";      # update after release (same format as perl $])
-#$IS_DEVEL_BUILD = 1;        # change for release versions
+$VERSION = "3.001007";      # update after release (same format as perl $])
+$IS_DEVEL_BUILD = 1;        # change for release versions
 
 @ISA = qw();
 
--- spamassassin.spec
+++ spamassassin.spec
@@ -1,5 +1,5 @@
 # the version in the tar name
-%define real_version 3.1.6
+%define real_version 3.1.7
 # the display version number
 %define version %{real_version}
 
