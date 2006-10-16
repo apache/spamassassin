7a7ab9b56f173601642295eb5c3a32ce4f65dc9d
Justin Mason <jm@apache.org>
2006-10-16 13:28:25 +0000

    bug 4594: add possible workaround for the rare 'ordered child to accept, but they reported state '1'' bug; unclear if this works
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@464484 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/SpamdForkScaling.pm
+++ lib/Mail/SpamAssassin/SpamdForkScaling.pm
@@ -426,7 +426,13 @@ sub order_idle_child_to_accept {
     dbg("prefork: ordered $kid to accept");
 
     # now wait for it to say it's done that
-    return $self->wait_for_child_to_accept($sock);
+    my $ret = $self->wait_for_child_to_accept($kid, $sock);
+    if ($ret) {
+      return $ret;
+    } else {
+      # retry with another child
+      return $self->order_idle_child_to_accept();
+    }
 
   }
   else {
@@ -436,10 +442,11 @@ sub order_idle_child_to_accept {
 }
 
 sub wait_for_child_to_accept {
-  my ($self, $sock) = @_;
+  my ($self, $kid, $sock) = @_;
 
   while (1) {
     my $state = $self->read_one_message_from_child_socket($sock);
+
     if ($state == PFSTATE_BUSY) {
       return 1;     # 1 == success
     }
@@ -447,7 +454,12 @@ sub wait_for_child_to_accept {
       return undef;
     }
     else {
-      die "prefork: ordered child to accept, but child reported state '$state'";
+      warn "prefork: ordered child $kid to accept, but they reported state '$state', killing rogue";
+      $self->child_error_kill($kid, $sock);
+      $self->adapt_num_children();
+      sleep 1;
+
+      return undef;
     }
   }
 }
