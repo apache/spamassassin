5a9b1498980bd2238c004148ea0a54dc08381301
Justin Mason <jm@apache.org>
2007-06-01 11:55:11 +0000

    bug 5492: whitelist_bounce_relays was being applied to all messages, whether they should have been vbounce-whitelisted or not
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@543469 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/VBounce.pm
+++ lib/Mail/SpamAssassin/Plugin/VBounce.pm
@@ -119,7 +119,8 @@ sub check_whitelist_bounce_relays {
   # skip past the headers
   my $foundnlnl = 0;
   foreach my $line ($pristine =~ /^(.*)$/gm) {
-    if ($line =~ /^$/) {
+    # note: do not use any re match here, it'll reset /g
+    if ($line eq "" || $line eq "\n" || $line eq "\r\n") {
       $foundnlnl = 1; last;
     }
   }
