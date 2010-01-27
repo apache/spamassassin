fcfe838c43a7410730ef17014dd79d311486a9bf
Mark Martinec <mmartinec@apache.org>
2010-01-27 11:43:13 +0000

    Bug 6307 - exists in header rule fails if underscore present in pattern
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.3@903615 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Conf/Parser.pm
+++ lib/Mail/SpamAssassin/Conf/Parser.pm
@@ -1127,7 +1127,8 @@ sub add_test {
   }
   if ($type == $Mail::SpamAssassin::Conf::TYPE_HEAD_TESTS)
   {
-    if ($text =~ /^!?defined\([A-Za-z][A-Za-z0-9-]*\)$/) {
+    # RFC 5322 section 3.6.8, ftext printable US-ASCII chars not including ":"
+    if ($text =~ /^!?defined\([!-9;-\176]+\)$/) {
       # fine, implements 'exists:'
     } else {
       my ($pat) = ($text =~ /^\s*\S+\s*(?:\=|\!)\~\s*(\S.*?\S)\s*$/);
