c49759fd68a4cc0396a556b20980d715b4b15be3
Mark Martinec <mmartinec@apache.org>
2010-01-27 14:24:09 +0000

    Bug 6310 - sa-learn --import gives Insecure dependency in open
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@903657 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/BayesStore/DBM.pm
+++ lib/Mail/SpamAssassin/BayesStore/DBM.pm
@@ -1438,6 +1438,9 @@ sub perform_upgrade {
     # bayes directory
     my $main = $self->{bayes}->{main};
     my $path = $main->sed_path($main->{conf}->{bayes_path});
+
+    # prevent dirname() from tainting the result, it assumes $1 is not tainted
+    local($1,$2,$3);  # Bug 6310
     my $dir = dirname($path);
 
     # make temporary copy since old dbm and new dbm may have same name
