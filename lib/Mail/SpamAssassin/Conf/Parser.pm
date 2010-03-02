654a235cea4e44070015b5abd06585d95f164d6c
Mark Martinec <mmartinec@apache.org>
2010-03-02 17:24:37 +0000

    Bug 6342: make test failure on if_can under perl 5.6
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.3@918118 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Conf/Parser.pm
+++ lib/Mail/SpamAssassin/Conf/Parser.pm
@@ -542,8 +542,10 @@ sub cond_clause_plugin_loaded {
 sub cond_clause_can {
   my ($self, $method) = @_;
 
+  local($1,$2);
   if ($method =~ /^(.*)::([^:]+)$/) {
-    return UNIVERSAL::can($1, $2);
+    my($module, $meth) = ($1, $2);
+    return UNIVERSAL::can($module, $meth);
   } else {
     $self->lint_warn("bad 'if' line, cannot find '::' in can($method), ".
                 "in \"$self->{currentfile}\"", undef);
