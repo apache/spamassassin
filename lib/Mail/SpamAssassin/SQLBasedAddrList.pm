0227317234e339773092b9b19a7b425b52d8ab35
Joe Quinn <jquinn@apache.org>
2015-08-27 14:22:57 +0000

    Ugly fix for TxRep data being updated incorrectly
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1698165 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/SQLBasedAddrList.pm
+++ lib/Mail/SpamAssassin/SQLBasedAddrList.pm
@@ -220,6 +220,7 @@ sub get_addr_entry {
     }
     push(@args, @signedby);
   }
+  $sql .= " ORDER BY last_hit";
   my $sth = $self->{dbh}->prepare($sql);
   my $rc = $sth->execute($self->{_username}, @args);
 
@@ -236,8 +237,8 @@ sub get_addr_entry {
     # an author domain and by a remailer)?  for now just take an average
     while ( defined($aryref = $sth->fetchrow_arrayref()) ) {
       if (defined $entry->{count} && defined $aryref->[1]) {
-        $entry->{count} += $aryref->[0];
-        $entry->{totscore} += $aryref->[1];
+        $entry->{count} = $aryref->[0];
+        $entry->{totscore} = $aryref->[1];
       }
       $entry->{exists_p} = 1;
       $cnt++;
