60d3506ed18a9b673317432ae2c6c7548179e79f
Karsten Braeckelmann <kb@apache.org>
2017-07-23 02:54:15 +0000

    bug 7155: Fix spamc longoptions prefix-substring handling.
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1802715 13f79535-47bb-0310-9956-ffa450edef68

--- spamc/getopt.c
+++ spamc/getopt.c
@@ -251,7 +251,7 @@ spamc_getopt_long(int argc, char * const argv[],
 	 /* changed to longopts[i].name[0] == 0 - bug 7148 */
          if((longopts[i].name == NULL) || (longopts[i].name[0] == 0))
             return(longoptiserr(argc, argv, spamc_optind-1, OPTERRNF));
-         if((memcmp(longopt+2, longopts[i].name, longoptlen)) == 0) {
+         if(((memcmp(longopt+2, longopts[i].name, longoptlen)) == 0) && (strlen(longopts[i].name) == longoptlen)) {
             *longindex = i;
             if(longopts[i].has_arg == required_argument) {
                if(((spamc_optind >= argc) || (!argv[spamc_optind]) || (argv[spamc_optind][0] == '-')) && 
