4e7e162d52586496cb04629d5e3e6c8f761c53d9
Kevin A. McGrail <kmcgrail@apache.org>
2018-08-25 23:31:00 +0000

    Addig tag for LASTEXTERNALIP - Bug 7334
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.4@1839147 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -1716,6 +1716,10 @@ sub extract_message_metadata {
   $self->set_tag('RELAYSUNTRUSTED', $self->{relays_untrusted_str});
   $self->set_tag('RELAYSINTERNAL',  $self->{relays_internal_str});
   $self->set_tag('RELAYSEXTERNAL',  $self->{relays_external_str});
+  my $lasthop = $self->{relays_external}->[0];
+  if ($lasthop) {
+    $self->set_tag('LASTEXTERNALIP',  $lasthop->{ip});
+  }
   $self->set_tag('LANGUAGES', $self->{msg}->get_metadata("X-Languages"));
 
   # This should happen before we get called, but just in case.
