1adbe0ef87aca932b7327c3893f588ef42712857
Henrik Krohns <hege@apache.org>
2018-09-28 07:30:59 +0000

    Sorry, more hasty fixes
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1842215 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/Check.pm
+++ lib/Mail/SpamAssassin/Plugin/Check.pm
@@ -222,7 +222,7 @@ sub check_main {
   if ($pms->{conf}->{dns_block_rule}) {
     foreach my $rule (keys %{$pms->{conf}->{dns_block_rule}}) {
       next if !$pms->{tests_already_hit}->{$rule}; # hit?
-      foreach my $domain (keys %{$conf_ref->{dns_block_rule}{$rule}}) {
+      foreach my $domain (keys %{$pms->{conf}->{dns_block_rule}{$rule}}) {
         my $blockfile = $self->{main}->sed_path("__global_state_dir__/dnsblock_$domain");
         next if -f $blockfile; # no need to warn and create again..
         warn("check: dns_block_rule $rule hit, creating $blockfile\n");
