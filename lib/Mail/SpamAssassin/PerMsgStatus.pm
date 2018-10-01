9c99f6fddded4a17feb41182bac9bc0ebf981ebf
Henrik Krohns <hege@apache.org>
2018-10-01 10:44:52 +0000

    Fix doc typo
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1842467 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -2575,7 +2575,7 @@ sub clear_test_state {
     %{$self->{test_log_msgs}} = ();
 }
 
-# internal API, called only by get_hit()
+# internal API, called only by got_hit()
 # TODO: refactor and merge this into that function
 sub _handle_hit {
     my ($self, $rule, $score, $area, $ruletype, $desc) = @_;
