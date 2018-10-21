49452bc1b28c9a7ea4fee9989b82ac2626b69c39
Giovanni Bechis <gbechis@apache.org>
2018-10-21 12:10:40 +0000

    Add last_hit to awl table as well bz #7631
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.4@1844485 13f79535-47bb-0310-9956-ffa450edef68

--- sql/awl_mysql.sql
+++ sql/awl_mysql.sql
@@ -5,5 +5,6 @@ CREATE TABLE awl (
   msgcount int(11) NOT NULL default '0',
   totscore float NOT NULL default '0',
   signedby varchar(255) NOT NULL default '',
+  last_hit timestamp NOT NULL default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (username,email,signedby,ip)
 ) ENGINE=InnoDB;
