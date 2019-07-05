788bd53151e725eeef70e69d0425ce77e9dd0129
Giovanni Bechis <gbechis@apache.org>
2019-07-05 17:40:10 +0000

    sync dependencies check with reality
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.4@1862625 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Util/DependencyInfo.pm
+++ lib/Mail/SpamAssassin/Util/DependencyInfo.pm
@@ -125,11 +125,27 @@ our @OPTIONAL_MODULES = (
   address forgery and make it easier to identify spams.',
 },
 {
+  module => 'GeoIP2::Database::Reader',
+  version => 0,
+  desc => 'Used by the RelayCountry plugin (not enabled by default) to
+  determine the domain country codes of each relay in the path of an email. 
+  Also used by the URILocalBL plugin (not enabled by default) to provide ISP
+  and Country code based filtering.',
+},
+{
   module => 'Geo::IP',
   version => 0,
   desc => 'Used by the RelayCountry plugin (not enabled by default) to determine
   the domain country codes of each relay in the path of an email.  Also used by 
   the URILocalBL plugin to provide ISP and Country code based filtering.',
+},
+{
+  module => 'IP::Country::DB_File',
+  version => 0,
+  desc => 'Used by the RelayCountry plugin (not enabled by default) to
+  determine the domain country codes of each relay in the path of an email. 
+  Also used by the URILocalBL plugin (not enabled by default) to provide
+  Country code based filtering.',
 },
 {
   module => 'Net::CIDR::Lite',
@@ -262,6 +278,18 @@ our @OPTIONAL_MODULES = (
   version => 0,
   desc => 'BSD::Resource provides BSD process resource limit and priority 
   functions.  It is used by the optional ResourceLimits Plugin.',
+},
+{
+  module => 'Archive::Zip',
+  version => 0,
+  desc => 'Archive::Zip provides an interface to ZIP archive files.
+  It is used by the optional OLEMacro Plugin.',
+},
+{
+  module => 'IO::String',
+  version => 0,
+  desc => 'IO::String emulates file interface for in-core strings.
+  It is used by the optional OLEMacro Plugin.',
 },
 );
 
