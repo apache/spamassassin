13d242664a02e3d469140688ba37530a342396c0
Henrik Krohns <hege@apache.org>
2019-08-03 14:40:38 +0000

    Fix _URIDOMAINS_ duplicates (Bug 6966)
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1864340 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/URIDNSBL.pm
+++ lib/Mail/SpamAssassin/Plugin/URIDNSBL.pm
@@ -480,10 +480,10 @@ sub check_dnsbl {
     }
   }
 
-  my @hnames = keys %hostlist;
+  my @hnames = sort keys %hostlist;
   $pms->set_tag('URIHOSTS',
                 @hnames == 1 ? $hnames[0] : \@hnames)  if @hnames;
-  my @dnames = values %hostlist;
+  my @dnames = do { my %seen; grep { !$seen{$_}++ } sort values %hostlist };
   $pms->set_tag('URIDOMAINS',
                 @dnames == 1 ? $dnames[0] : \@dnames)  if @dnames;
 
