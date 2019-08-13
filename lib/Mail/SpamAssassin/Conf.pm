cdd589c982b1494cc4f80774ad82058340e08cb5
Henrik Krohns <hege@apache.org>
2019-08-13 05:14:01 +0000

    Fix report_wrap_width doc
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1865008 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Conf.pm
+++ lib/Mail/SpamAssassin/Conf.pm
@@ -1095,7 +1095,7 @@ the original mail into tagged messages.
     }
   });
 
-=item report_wrap_width (default: 70) 
+=item report_wrap_width (default: 75) 
 
 This option sets the wrap width for description lines in the X-Spam-Report
 header, not accounting for tab width. 
