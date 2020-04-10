86516a33b2e0bb88269e223c7a7e40ba15614bf7
Henrik Krohns <hege@apache.org>
2020-04-10 07:51:51 +0000

    Bug 7803 - SQL schema of userpref table, value too short
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.4@1876348 13f79535-47bb-0310-9956-ffa450edef68

--- sql/userpref_mysql.sql
+++ sql/userpref_mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE userpref (
   username varchar(100) NOT NULL default '',
   preference varchar(50) NOT NULL default '',
-  value varchar(100) NOT NULL default '',
+  value varchar(255) NOT NULL default '',
   prefid int(11) NOT NULL auto_increment,
   PRIMARY KEY  (prefid),
   KEY username (username)
--- sql/userpref_pg.sql
+++ sql/userpref_pg.sql
@@ -2,6 +2,6 @@ CREATE TABLE userpref (
   prefid bigserial NOT NULL unique primary key,
   username varchar(100) NOT NULL,
   preference varchar(50) NOT NULL,
-  value varchar(100) NOT NULL
+  value varchar(255) NOT NULL
 );
 CREATE INDEX userpref_username_idx ON userpref(username);
