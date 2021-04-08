e2698f001ec718386331d50f74279dd011dd2386
Sidney Markowitz <sidney@apache.org>
2021-04-08 04:01:39 +0000

    preparing to release 3.4.6-rc1
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/branches/3.4@1888492 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -88,14 +88,14 @@ use Cwd;
 use Config;
 
 our $VERSION = "3.004006";      # update after release (same format as perl $])
-our $IS_DEVEL_BUILD = 1;        # 1 for devel build 
-#our $IS_DEVEL_BUILD = 0;        # 0 for release versions including rc & pre releases
+#our $IS_DEVEL_BUILD = 1;        # 1 for devel build 
+our $IS_DEVEL_BUILD = 0;        # 0 for release versions including rc & pre releases
 
 
 # Used during the prerelease/release-candidate part of the official release
 # process. If you hacked up your SA, you should add a version_tag to your .cf
 # files; this variable should not be modified.
-our @EXTRA_VERSION = qw();
+our @EXTRA_VERSION = qw(rc1);
 
 our @ISA = qw();
 
