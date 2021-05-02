ade996bb41e3f51ca82536379d6598faa508dcde
Henrik Krohns <hege@apache.org>
2021-05-02 07:44:06 +0000

    Improve logging
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1889387 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/ReplaceTags.pm
+++ lib/Mail/SpamAssassin/Plugin/ReplaceTags.pm
@@ -135,7 +135,7 @@ sub finish_parsing_end {
         my $pre = $conf->{replace_pre}->{$pre_name};
         if ($pre) {
           s{($start.+?$end)}{$pre$1}  for @re;
-         }
+        }
       }
       if ($post_name) {
         my $post = $conf->{replace_post}->{$post_name};
@@ -172,12 +172,15 @@ sub finish_parsing_end {
       # do the actual replacement
       my ($rec, $err) = compile_regexp($re, 0);
       if (!$rec) {
-        info("replacetags: regexp compilation failed '$re': $err");
+        info("replacetags: regexp compilation failed for $rule: '$re': $err");
         next;
       }
       $conf->{test_qrs}->{$rule} = $rec;
-      #dbg("replacetags: replaced $rule: '$origre' => '$re'");
-      dbg("replacetags: replaced $rule");
+      if (would_log('dbg','replacetags') > 1) {
+        dbg("replacetags: replaced $rule: '$origre' => '$re'");
+      } else {
+        dbg("replacetags: replaced $rule");
+      }
     } else {
       dbg("replacetags: nothing was replaced in $rule");
     }
