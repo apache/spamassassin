ec614bd0dd5309147a449f17494f017f60e1be54
Kent Oyer <fkento@apache.org>
2025-01-15 00:11:01 +0000

    Eliminate arc_author_domains because it's the same as dkim_author_domains
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1923136 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/DKIM.pm
+++ lib/Mail/SpamAssassin/Plugin/DKIM.pm
@@ -807,7 +807,7 @@ sub _check_dkim_signed_by {
 }
 
 sub _get_authors {
-  my ($self, $pms, $sig_type) = @_;
+  my ($self, $pms) = @_;
 
   # Note that RFC 5322 permits multiple addresses in the From header field,
   # and according to RFC 5617 such message has multiple authors and hence
@@ -820,8 +820,8 @@ sub _get_authors {
     # be tolerant, ignore trailing WSP after a domain name
     $author_domains{lc $1} = 1  if /\@([^\@]+?)[ \t]*\z/s;
   }
-  $pms->{"${sig_type}_author_addresses"} = \@authors;       # list of full addresses
-  $pms->{"${sig_type}_author_domains"} = \%author_domains;  # hash of their domains
+  $pms->{"dkim_author_addresses"} = \@authors;       # list of full addresses
+  $pms->{"dkim_author_domains"} = \%author_domains;  # hash of their domains
 }
 
 sub _check_dkim_signature {
@@ -907,8 +907,7 @@ sub _check_dkim_signature {
 sub _check_signature {
   my($self, $pms, $verifier, $type, $signatures) = @_;
 
-  my $sig_type = lc $type;
-  $self->_get_authors($pms, $sig_type)  if !$pms->{"${sig_type}_author_addresses"};
+  $self->_get_authors($pms)  if !$pms->{"dkim_author_addresses"};
 
   my(@valid_signatures);
   my $conf = $pms->{conf};
@@ -1003,7 +1002,7 @@ sub _check_valid_signature {
   my($self, $pms, $verifier, $type, $signatures) = @_;
 
   my $sig_type = lc $type;
-  $self->_get_authors($pms, $sig_type)  if !$pms->{"${sig_type}_author_addresses"};
+  $self->_get_authors($pms)  if !$pms->{"dkim_author_addresses"};
 
   my(@valid_signatures);
   my $conf = $pms->{conf};
@@ -1044,7 +1043,7 @@ sub _check_valid_signature {
         # can be undefined on a broken signature with missing required tags
       } else {
         $d = lc $d;
-        if ($pms->{"${sig_type}_author_domains"}->{$d}) {  # SDID matches author domain
+        if ($pms->{dkim_author_domains}->{$d}) {  # SDID matches author domain
           $pms->{"${sig_type}_has_any_author_sig"}->{$d} = 1;
           if ($valid && !$expired &&
               $key_size && $key_size >= $minimum_key_bits) {
@@ -1081,7 +1080,7 @@ sub _check_valid_signature {
               $signature->algorithm, scalar($signature->canonicalization),
               $key_size ? "key_bits=$key_size" : "unknown key size",
               ($sig_result_supported ? $signature : $verifier)->result ),
-            defined $d && $pms->{arc_author_domains}->{$d}
+            defined $d && $pms->{dkim_author_domains}->{$d}
               ? 'matches author domain'
               : 'does not match author domain',
           );
@@ -1143,7 +1142,7 @@ sub _check_dkim_adsp {
   $pms->{dkim_adsp} = {};  # a hash: author_domain => adsp
   my $practices_as_string = '';
 
-  $self->_get_authors($pms, 'dkim')  if !$pms->{dkim_author_addresses};
+  $self->_get_authors($pms)  if !$pms->{dkim_author_addresses};
 
   # collect only fully qualified domain names, allow '-', think of IDN
   my @author_domains = grep { /.\.[a-z-]{2,}\z/si }
@@ -1296,7 +1295,7 @@ sub _check_dkim_welcomelist {
 
   $pms->{welcomelist_checked} = 1;
 
-  $self->_get_authors($pms, 'dkim')  if !$pms->{dkim_author_addresses};
+  $self->_get_authors($pms)  if !$pms->{dkim_author_addresses};
 
   my $authors_str = join(", ", @{$pms->{dkim_author_addresses}});
   if ($authors_str eq '') {
