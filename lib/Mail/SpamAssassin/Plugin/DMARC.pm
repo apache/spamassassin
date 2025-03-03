900494507d2b8b73826ade5dc434073c5f31f037
Giovanni Bechis <gbechis@apache.org>
2025-03-03 14:44:36 +0000

    do not try to access published policies if there is no DMARC policy bz #8319
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1924149 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/DMARC.pm
+++ lib/Mail/SpamAssassin/Plugin/DMARC.pm
@@ -391,7 +391,7 @@ sub _check_dmarc {
     }
   }
 
-  if($result->published->can('stringify')) {
+  if(defined $result and ($result->result ne 'none') and ($result->published->can('stringify'))) {
     dbg("Found DMARC record \"" . $result->published->stringify . "\" for domain $mfrom_domain");
   }
 
