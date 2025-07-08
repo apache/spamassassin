7e2272bf5446ef13329b4520a6c70fb822ee09d4
Giovanni Bechis <gbechis@apache.org>
2025-07-08 07:05:39 +0000

    improve regexp matches
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1927070 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/Redirectors.pm
+++ lib/Mail/SpamAssassin/Plugin/Redirectors.pm
@@ -925,7 +925,7 @@ sub recursive_lookup {
 	    dbg("Connection timeout checking $redir_url");
 	  }
         } elsif($rcode eq 200) {
-	  if((defined $response->content) and ($response->content =~ /http-equiv=["']?refresh["']?.*?content=["']?(\d+);\s+url=((?:https?:\/\/)?[^"'\/\\]+)["']?/i)) {
+	  if((defined $response->content) and ($response->content =~ /http-equiv=["']?refresh["']?.{1,64}?content=["']?(\d+);\s+url=((?:https?:\/\/)?[^"'\/\\]+)["']?/is)) {
 	    my $delay = $1;
 	    $location = $2;
 	    if($delay eq 0) {
