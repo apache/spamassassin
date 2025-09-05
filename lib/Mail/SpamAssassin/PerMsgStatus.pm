893e6dc80144942d5b1df3f5cd2472e5d102ec7b
Giovanni Bechis <gbechis@apache.org>
2025-09-05 06:36:52 +0000

    skip CNAME checks if dns_max_cname_cache == 0
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1928240 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/PerMsgStatus.pm
+++ lib/Mail/SpamAssassin/PerMsgStatus.pm
@@ -2910,7 +2910,7 @@ sub add_uri_detail_list {
     }
 
     my $dns_max_cname_cache = $self->{main}->{conf}->{dns_max_cname_cache};
-    if($self->is_dns_available() and (not defined $self->{dns_cname_cache} or scalar %{$self->{dns_cname_cache}} < $dns_max_cname_cache)) {
+    if($self->is_dns_available() and ($dns_max_cname_cache > 0) and (not defined $self->{dns_cname_cache} or scalar %{$self->{dns_cname_cache}} < $dns_max_cname_cache)) {
       # XXX we cannot call bgsend_and_start_lookup,
       # otherwise get_uri_detail_list() might not
       # return domains extracted from CNAME dns queries
