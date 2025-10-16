849d4d5274a70db7cc669ee1e055c1a4db119d5b
Giovanni Bechis <gbechis@apache.org>
2025-10-16 13:39:33 +0000

    avoid a warning
    git-svn-id: https://svn.apache.org/repos/asf/spamassassin/trunk@1929175 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Mail/SpamAssassin/Plugin/Redirectors.pm
+++ lib/Mail/SpamAssassin/Plugin/Redirectors.pm
@@ -1003,12 +1003,14 @@ sub recursive_lookup {
   my %paramseen;
   my $denorm_location = $location;
   my ($hostpart, $querystring) = split /\?/, $location, 2;
-  my @params = split /&|%26/, $querystring;
-  my @unique_params = grep { !$paramseen{$_}++ } @params;
-  my $nquerystring = join '&', @unique_params;
-  $location = $hostpart . '?' . $nquerystring;
-  if($denorm_location ne $location) {
-    dbg("Normalizing redirector parameters from $denorm_location to $location"); 
+  if(defined $querystring) {
+    my @params = split /&|%26/, $querystring;
+    my @unique_params = grep { !$paramseen{$_}++ } @params;
+    my $nquerystring = join '&', @unique_params;
+    $location = $hostpart . '?' . $nquerystring;
+    if($denorm_location ne $location) {
+      dbg("Normalizing redirector parameters from $denorm_location to $location"); 
+    }
   }
 
   my ($domain, $host) = $self->{main}->{registryboundaries}->uri_to_domain($location);
